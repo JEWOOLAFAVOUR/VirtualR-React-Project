bb75ef9656de1df637ed2943660f9c85acb8b849
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-10-26 06:04:39 +0100

    done with one

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,46 @@ import { resourcesLinks, platformLinks, communityLinks } from '../constants'
 
 const Footer = () => {
     return (
-        <div>
-
-        </div>
+        <footer className='mt-20 border-t py-10 border-neutral-700'>
+            <div className='grid grid-cols-2 lg:grid-cols-3 gap-4'>
+                <div>
+                    <h3 className='text-xl font-semibold mb-4'>Resources</h3>
+                    <ul className='space-y-2'>
+                        {
+                            resourcesLinks.map((data, index) => (
+                                <li key={index}>
+                                    <a href={data.href} className='text-neutral-300 hover:text-white'>{data.text}</a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div>
+                    <h3 className='text-xl font-semibold mb-4'>Platform</h3>
+                    <ul className='space-y-2'>
+                        {
+                            platformLinks.map((data, index) => (
+                                <li key={index}>
+                                    <a href={data.href} className='text-neutral-300 hover:text-white'>{data.text}</a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div>
+                    <h3 className='text-xl font-semibold mb-4'>Community</h3>
+                    <ul className='space-y-2'>
+                        {
+                            communityLinks.map((data, index) => (
+                                <li key={index}>
+                                    <a href={data.href} className='text-neutral-300 hover:text-white'>{data.text}</a>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+        </footer>
     )
 }
 
