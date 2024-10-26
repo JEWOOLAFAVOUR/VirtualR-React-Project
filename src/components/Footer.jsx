0a6aecd9edcd3bb68c23f82de152c499fa9ef8fa
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-10-26 05:55:28 +0100

    starting footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { resourcesLinks, platformLinks, communityLinks } from '../constants'
+
+const Footer = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Footer
